16037cb00a623e4700f0a75a3f96838388351ed8
sainithinkatta <sainithinkatta24@gmail.com>
2025-02-03 17:59:02 -0600

    added tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 <template>
   <div>
-    <h1> Vue Project </h1>
+    <h1 class="text-3xl font-bold underline"> 
+      Vue Project 
+    </h1>
   </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+  @import "tailwindcss";
 </style>
