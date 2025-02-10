5e4464f8d4ba14e13d4396cac0f406d341d9262d
sainithinkatta <sainithinkatta24@gmail.com>
2025-02-10 14:37:31 -0600

    updated css

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -9,10 +9,10 @@
             :key="index"
             @click="selectTab(index)"
             :class="[
-              'whitespace-nowrap pb-2 px-1 border-b-2 font-medium text-sm transition-colors duration-200',
+              'whitespace-nowrap px-1 border-b-2 font-medium text-sm transition-colors duration-200',
               activeTab === index
-                ? 'bg-gradient-to-r from-blue-600 to-purple-700 text-white pb-2 px-4 py-2 rounded-lg hover:bg-blue-600  cursor-pointer'
-                : 'border-transparent text-gray-500 hover:text-gray-700 pb-2 hover:border-gray-300'
+                ? 'bg-gradient-to-r from-blue-600 to-purple-700 text-white px-4 py-2 rounded-lg hover:bg-blue-600  cursor-pointer'
+                : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
             ]"
             :aria-current="activeTab === index ? 'page' : undefined"
           >
