941afde7b39a10b6b19d3cb0cb068ec61defc4f9
sainithinkatta <sainithinkatta24@gmail.com>
2025-02-10 14:39:51 -0600

    updated css

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -9,7 +9,7 @@
             :key="index"
             @click="selectTab(index)"
             :class="[
-              'whitespace-nowrap px-1 border-b-2 font-medium text-sm transition-colors duration-200',
+              'whitespace-nowrap px-1 border-b-2 font-medium text-sm transition-colors duration-200  cursor-pointer',
               activeTab === index
                 ? 'bg-gradient-to-r from-blue-600 to-purple-700 text-white px-4 py-2 rounded-lg hover:bg-blue-600  cursor-pointer'
                 : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
